b010a94ad5d96fc488a09959bef5e6762caebde9
marcelohiginoduarte <marcelihigino@gmail.com>
2025-02-21 15:19:02 -0300

    Criando o acompanhamento por projeto de custos operacionais

--- apps/acompanhamentosfinanceiroprojeto/urls.py
+++ apps/acompanhamentosfinanceiroprojeto/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('despesa/proheto', views.criar_despesa_projeto, name='criardespesaprojeto'),
+    path('despesa/projeto', views.criar_despesa_projeto, name='criardespesaprojeto'),
+    path('despesa/todas', views.ver_despesas_projeto, name='criardespesaprojetotodas'),
+    path('despesa/atualizar/<int:pk>', views.AtualizarCustos.as_view(), name='atualizar_custos'),
+    path('despesa/deletar/<int:pk>', views.DeletarCustos.as_view(), name='deletar_custos'),
 ]
