d0c28380b1863689804965bedcd9c929989f3f04
marcelohiginoduarte <marcelihigino@gmail.com>
2025-05-15 15:36:34 -0300

    calendarys

--- apps/acompanhamentosfinanceiroprojeto/views.py
+++ apps/acompanhamentosfinanceiroprojeto/views.py
@@ -23,7 +23,7 @@ def criar_despesa_projeto(request):
         form = CriarAcompanhamentoDespesasForms(request.POST, request.FILES)
         if form.is_valid():
             form.save()
-            return redirect('criardespesaprojeto')
+            return redirect('criardespesaprojetotodas')
     else:
         form = CriarAcompanhamentoDespesasForms()
 
